4f1f14e2b535e26cb5df7b4caa13ff063dc81275
Darina <dashunkaleo@yandex.ru>
2021-07-06 10:38:01 +0300

    scroll

--- scripts/script.js
+++ scripts/script.js
@@ -13,15 +13,40 @@ headerCityButton.addEventListener('click', () => {
     localStorage.setItem('lomoda-location', city)
 })
 
+//scroll
+
+const disableScroll = () => {
+    const scrollWidth = window.innerWidth - document.body.offsetWidth;
+    document.body.dbScrollY = window.scrollY;
+    document.body.style.cssText = `
+    position: fixed;
+    top: ${-window.scrollY}px;
+    width: 100%;
+    height: 1vh;
+    overflow: hidden;
+    padding-right: ${scrollWidth}px; 
+    `;
+    console.log(scrollWidth);
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY,
+    })
+};
+
 //cart modal 
 
 subheaderCart.addEventListener('click', () => {
     cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
 })
 
 cartOverlay.addEventListener('click', e => {
     const target = e.target;
     if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
         cartOverlay.classList.remove('cart-overlay-open');
+        enableScroll();
     }
 })
